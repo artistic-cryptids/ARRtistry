1cd33c44a7702f977c8a016a60b2e66dd29dd9ec
James Dalboth <James.Dalboth@gmail.com>
2019-10-13 19:13:22 +0100

    Revert "Upgradeable contracts"

--- contracts/Arrtistry.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.0;
-
-import "zos-lib/contracts/Initializable.sol";
-
-contract Arrtistry is Initializable {
-    // initialise function acts as constructor
-    function initialize() public initializer {}
-}
