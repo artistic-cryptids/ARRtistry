ca109a08be2115bd2433968c19ad79ac130a9457
JamesDalboth <james.dalboth@gmail.com>
2019-10-14 19:47:40 +0100

    Fix style issues

--- contracts/VersionResolver.sol
+++ contracts/VersionResolver.sol
@@ -1,45 +1,46 @@
 pragma solidity ^0.5.0;
 
 contract VersionResolver {
-    address owner;
+  address private owner;
 
-    struct Contract {
-        address addr;
-        string abi;
-    }
+  struct Contract {
+    address addr;
+    string abi;
+  }
 
-    mapping(string=>Contract) contracts;
+  mapping(string => Contract) private contracts;
 
-    modifier only_owner() {
-        require(msg.sender == owner);
-        _;
-    }
+  modifier only_owner() {
+    require(msg.sender == owner);
+    _;
+  }
 
-    constructor() public {
-        owner = msg.sender;
-    }
+  constructor() public {
+    owner = msg.sender;
+  }
 
-    // Our resolver supports (supportsInterface & addr) interfaces
-    function supportsInterface(bytes4 interfaceID) pure public returns (bool) {
-        return interfaceID == 0x3b3b57de
-            || interfaceID == 0x01ffc9a7;
-    }
+  // Our resolver supports (supportsInterface & addr) interface
+  function supportsInterface(bytes4 interfaceID) public pure returns (bool) {
+    return interfaceID == 0x3b3b57de
+    || interfaceID == 0x01ffc9a7;
+  }
 
-    function addr(bytes32 nodeID) public view returns (address) {
-        return address(this);
-    }
+  // solhint-disable-next-line no-unused-vars
+  function addr(bytes32 nodeID) public view returns (address) {
+    return address(this);
+  }
 
-    function releaseVersion(string memory _version, string memory _abi, address _addr) public only_owner {
-        Contract memory contr = Contract(_addr, _abi);
-        contracts[_version] = contr;
-        contracts['latest'] = contr;
-    }
+  function releaseVersion(string memory _version, string memory _abi, address _addr) public only_owner {
+    Contract memory contr = Contract(_addr, _abi);
+    contracts[_version] = contr;
+    contracts["latest"] = contr;
+  }
 
-    function getAddr(string memory _version) public view returns (address) {
-        return contracts[_version].addr;
-    }
+  function getAddr(string memory _version) public view returns (address) {
+    return contracts[_version].addr;
+  }
 
-    function getAbi(string memory _version) public view returns (string memory) {
-        return contracts[_version].abi;
-    }
+  function getAbi(string memory _version) public view returns (string memory) {
+    return contracts[_version].abi;
+  }
 }
