203f1d44590c101cf9708f38d7a89b28f43ff262
JamesDalboth <james.dalboth@gmail.com>
2019-10-14 17:40:15 +0100

    Upgrade to solidity version 0.5.0 for VersonResolver

--- contracts/VersionResolver.sol
+++ contracts/VersionResolver.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.5.0;
 
 contract VersionResolver {
     address owner;
@@ -11,35 +11,35 @@ contract VersionResolver {
     mapping(string=>Contract) contracts;
 
     modifier only_owner() {
-        if(msg.sender != owner) throw;
+        require(msg.sender == owner);
         _;
     }
 
-    function VersionResolver() {
+    constructor() public {
         owner = msg.sender;
     }
 
     // Our resolver supports (supportsInterface & addr) interfaces
-    function supportsInterface(bytes4 interfaceID) constant view returns (bool) {
+    function supportsInterface(bytes4 interfaceID) pure public returns (bool) {
         return interfaceID == 0x3b3b57de
             || interfaceID == 0x01ffc9a7;
     }
 
-    function addr(bytes32 nodeID) constant view returns (address) {
+    function addr(bytes32 nodeID) public view returns (address) {
         return address(this);
     }
 
-    function releaseVersion(string version, address addr, string abi) only_owner {
-        Contract contr = Contract(addr, abi);
-        contracts[version] = contr;
+    function releaseVersion(string memory _version, string memory _abi, address _addr) public only_owner {
+        Contract memory contr = Contract(_addr, _abi);
+        contracts[_version] = contr;
         contracts['latest'] = contr;
     }
 
-    function getVersion(string version) constant view returns (address) {
-        return contracts[version];
+    function getAddr(string memory _version) public view returns (address) {
+        return contracts[_version].addr;
     }
 
-    function() {
-        throw;
+    function getAbi(string memory _version) public view returns (string memory) {
+        return contracts[_version].abi;
     }
 }
