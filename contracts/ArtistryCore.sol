d4ea89235310cd12b2eb39264da59d1dc033d770
Matthew Malarkey <mm5917@ic.ac.uk>
2019-10-15 13:01:26 +0100

    Core is now a full ERC721 token. Allow multiple moderators.

--- contracts/ArtistryCore.sol
+++ contracts/ArtistryCore.sol
@@ -1,18 +1,23 @@
 pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 
 /**
  * @title ArtistryCore
- * @dev Core functionality of the platform, registering users, artists and DACS
+ * @dev Core functionality of the platform
  */
-contract ArtistryCore is Ownable {
+contract ArtistryCore is Ownable, ERC721Full {
 
   // The address of the moderator
-  address dacs;
+  address[] public moderators;
+
+  constructor() ERC721Full("Artifact", "ART") public {
+    moderators.push(owner());
+  }
 
   // Update the moderator required to approve new art pieces etc.
-  function setModerator(address moderator) public onlyOwner {
-    dacs = moderator;
+  function addModerator(address moderator) public onlyOwner {
+    moderators.push(moderator);
   }
 }
