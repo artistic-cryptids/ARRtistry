27bbd403cec205d883a046b9c56846f7a76cec14
Matthew Ho <matthew_ho@hotmail.co.uk>
2019-10-15 20:54:17 +0100

    Set up drizzle with UI showing total number of users

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DrizzleContext } from 'drizzle-react';
+import User from './User';
 
-function App () {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => (
+  <DrizzleContext.Consumer>
+    {drizzleContext => {
+      const { drizzle, drizzleState, initialized } = drizzleContext;
 
-export default App;
+      if (!initialized) {
+        // TODO: Show a more user-friendly loading screen
+        return 'Loading...';
+      }
+
+      return (
+        <User drizzle={drizzle} drizzleState={drizzleState} />
+      );
+    }}
+  </DrizzleContext.Consumer>
+);
--- app/src/index.js
+++ app/src/index.js
@@ -4,7 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Drizzle, generateStore } from 'drizzle';
+import { DrizzleContext } from 'drizzle-react';
+import Users from './contracts/Users.json';
+
+const options = {
+  contracts: [Users],
+  web3: {
+    fallback: {
+      type: 'ws',
+      url: 'ws://127.0.0.1:8545',
+    },
+  },
+};
+
+const drizzleStore = generateStore(options);
+const drizzle = new Drizzle(options, drizzleStore);
+
+ReactDOM.render(
+  <DrizzleContext.Provider drizzle={ drizzle }>
+    <App />
+  </DrizzleContext.Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
