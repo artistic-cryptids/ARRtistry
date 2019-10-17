e56325169e6504ad3c85452993eb061950ae3df8
DJRHails <daniel@hails.info>
2019-10-17 15:08:23 +0100

    Remove safeMint

--- contracts/mocks/ERC721Mock.sol
+++ contracts/mocks/ERC721Mock.sol
@@ -7,14 +7,6 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
  * This mock just provides a public safeMint, mint, and burn functions for testing purposes
  */
 contract ERC721Mock is ERC721 {
-    function safeMint(address to, uint256 tokenId) public {
-        _safeMint(to, tokenId);
-    }
-
-    function safeMint(address to, uint256 tokenId, bytes memory _data) public {
-        _safeMint(to, tokenId, _data);
-    }
-
     function mint(address to, uint256 tokenId) public {
         _mint(to, tokenId);
     }
