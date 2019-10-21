c45ca64ed5bd80cb70301a2ba7adf3652649e0ea
JamesDalboth <james.dalboth@gmail.com>
2019-10-21 10:48:31 +0100

    Modify ArtifactRegistry to return tuple of values not struct

---
+++ app/src/components/ArtworkInfo.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Styles from '../theme';
+
+interface ArtworkItemProps {
+  artwork: any
+  id: any
+  classes: any
+}
+
+class ArtworkItem extends React.Component<ArtworkItemProps, {}> {
+  componentDidMount () {}
+
+  render (): React.ReactNode {
+    return <Grid container direction="row">
+         <ListItemAvatar>
+           {/* TODO: replace with thumbnail image?? */}
+           <Avatar alt={this.props.artwork.title}>{this.props.artwork.edition}</Avatar>
+         </ListItemAvatar>
+         <ListItemText
+           primary={this.props.artwork.title}
+           secondary={
+             <React.Fragment>
+               <Typography
+                 component="span"
+                 variant="body2"
+                 className={this.props.classes.inline}
+                 color="textPrimary"
+               >
+                 {this.props.artwork.artist_name}<br/>
+               </Typography>
+               {this.props.artwork.created}. {this.props.artwork.medium}
+             </React.Fragment>
+           }
+         />
+       </Grid>;
+  }
+}
+
+export default withStyles(Styles)(ArtworkItem);
