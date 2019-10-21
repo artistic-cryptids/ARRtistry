63f91dc208074c5ba9b51cdf800e74196558320d
Matthew Ho <matthew_ho@hotmail.co.uk>
2019-10-21 22:06:31 +0100

    Fix linting in ArtworkInfo.tsx

--- app/src/components/ArtworkInfo.tsx
+++ app/src/components/ArtworkInfo.tsx
@@ -8,37 +8,37 @@ import { withStyles } from '@material-ui/core/styles';
 import Styles from '../theme';
 
 interface ArtworkInfoProps {
-  artwork: any
-  id: any
-  classes: any
+  artwork: any;
+  id: any;
+  classes: any;
 }
 
 class ArtworkInfo extends React.Component<ArtworkInfoProps, {}> {
-  componentDidMount () {}
-
   render (): React.ReactNode {
-    return <Grid container direction="row">
-         <ListItemAvatar>
-           {/* TODO: replace with thumbnail image?? */}
-           <Avatar alt={this.props.artwork.title}>{this.props.artwork.edition}</Avatar>
-         </ListItemAvatar>
-         <ListItemText
-           primary={this.props.artwork.title}
-           secondary={
-             <React.Fragment>
-               <Typography
-                 component="span"
-                 variant="body2"
-                 className={this.props.classes.inline}
-                 color="textPrimary"
-               >
-                 {this.props.artwork.artist_name}<br/>
-               </Typography>
-               {this.props.artwork.created}. {this.props.artwork.medium}
-             </React.Fragment>
-           }
-         />
-       </Grid>;
+    return (
+      <Grid container direction="row">
+        <ListItemAvatar>
+          {/* TODO: replace with thumbnail image?? */}
+          <Avatar alt={this.props.artwork.title}>{this.props.artwork.edition}</Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary={this.props.artwork.title}
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                className={this.props.classes.inline}
+                color="textPrimary"
+              >
+                {this.props.artwork.artist_name}<br/>
+              </Typography>
+              {this.props.artwork.created}. {this.props.artwork.medium}
+            </React.Fragment>
+          }
+        />
+      </Grid>
+    );
   }
 }
 
