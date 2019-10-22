ebef0deb59b1d3d3a3549cb717590424ce739e54
Harry Black <harryjrblack@yahoo.co.uk>
2019-10-22 18:25:20 +0100

    Alter const artwork to reflect data important to DACS in ArtworkItem.tsx

--- app/src/components/ArtworkItem.tsx
+++ app/src/components/ArtworkItem.tsx
@@ -20,11 +20,13 @@ class ArtworkItem extends React.Component<ArtworkItemProps, ArtworkItemState> {
       .then((tokenId: any) => this.props.drizzle.contracts.ArtifactRegistry.methods.getArtifactForToken(tokenId).call())
       .then((artworkData: any) => {
         const artwork = {
-          title: artworkData[1],
-          medium: artworkData[2],
-          edition: artworkData[3],
-          created: artworkData[4],
-          metaUri: artworkData[5],
+          artistName: artworkData[1],
+          artistNationality: artworkData[2],
+          title: artworkData[3],
+          createdDate: artworkData[4],
+          medium: artworkData[5],
+          size: artworkData[6],
+          edition: artworkData[7]
         };
         this.setState({ artwork: artwork });
       })
