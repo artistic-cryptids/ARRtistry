a3d12b5cc308eae899d2870f734e35fc2bc98e92
DJRHails <daniel@hails.info>
2019-10-22 17:50:32 +0100

    Add example naming for governance

--- migrations/3_deploy_governance.js
+++ migrations/3_deploy_governance.js
@@ -1,6 +1,22 @@
 const Governance = artifacts.require('Governance');
-const PublicResolver = artifacts.require("@ensdomains/resolver/PublicResolver");
+// const PublicResolver = artifacts.require('PublicResolver');
+// const namehash = require('eth-ens-namehash');
+// const governanceName = 'governance.test';
 
 module.exports = async (deployer, network, accounts) => {
   await deployer.deploy(Governance);
+
+  // TODO: If necessary register
+  // const governance = await Governance.deployed();
+  // const resolver = await PublicResolver.deployed();
+  // await register(governanceName, resolver, governance);
 };
+
+
+// async function register(name, resolver, contract) {
+//   const addr = contract.address;
+//   const hash = namehash.hash(name);
+//   console.log('Registered', name, '(', hash, ') to', addr);
+//   await resolver.setAddr(hash, addr);
+//   console.log('Registered', name, '(', hash, ') to', addr);
+// }
