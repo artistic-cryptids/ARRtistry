a5c8ff21a6c360f1a82c15bfd22a68c52471b3d8
DJRHails <daniel@hails.info>
2019-10-22 17:28:23 +0100

    Initial ENS setup wallet naming

--- migrations/4_deploy_registry.js
+++ migrations/4_deploy_registry.js
@@ -1,19 +1,7 @@
 const ArtifactRegistry = artifacts.require('ArtifactRegistry');
 const Governance = artifacts.require('Governance');
-const PublicResolver = artifacts.require('PublicResolver');
-const FIFSRegistrar = artifacts.require('FIFSRegistrar');
-
-const newLabel = require('./helper/LoggedRegistration');
 
 module.exports = async (deployer, network, accounts) => {
   const governance = await Governance.deployed();
   await deployer.deploy(ArtifactRegistry, governance.address);
-
-  await newLabel(
-    'registry',
-    accounts[0],
-    await PublicResolver.deployed(),
-    await FIFSRegistrar.deployed(),
-    await ArtifactRegistry.deployed()
-  );
 };
