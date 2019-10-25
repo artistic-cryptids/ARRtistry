3b58d1c443210cd519a09a9c34424b56356bd9bf
Daniel Hails <daniel@hails.info>
2019-10-25 12:38:29 +0100

    Add Local ENS support on migration (#18)
    * Initial ENS setup wallet naming
    
    * Add example naming for governance
    
    * Register all deployed contracts with ENS
    
    * Lint the updated migrations
    
    * Initial ENS setup wallet naming
    
    * Register all deployed contracts with ENS
    
    * Lint the updated migrations
    
    * Bump version to 0.2.0 for ens feature
    
    * Remove node version 8 as causing issues
    
    * Put version in quotes
    
    * Fix npm start and npm test by ignoring the preflight check
    
    * Add quotes to node-version in frontend CI config
    
    * Remove unnecessary backend tests for Users and VersionResolver as these contracts no longer exist
    
    * Implement ENSResolver and import ENSRegistry and FIFSRegistrar
    
    * Change migrations to use ENSResolver and imported Registry, Registrar + Write test for ENSResolver
    
    * Added new line at end of file

--- migrations/3_deploy_registry.js
+++
@@ -1,7 +0,0 @@
-const ArtifactRegistry = artifacts.require('ArtifactRegistry');
-const Governance = artifacts.require('Governance');
-
-module.exports = async (deployer, network, accounts) => {
-  const governance = await Governance.deployed();
-  await deployer.deploy(ArtifactRegistry, governance.address);
-};
--- migrations/7_deploy_version_resolver.js
+++
@@ -1,5 +0,0 @@
-const VersionResolver = artifacts.require('VersionResolver');
-
-module.exports = (deployer, network, accounts) => {
-  deployer.deploy(VersionResolver);
-};
