fcceb9e4ed91ac2ce96ab69abc8d53aa55e8a7e8
Daniel Hails <daniel@hails.info>
2019-10-25 12:38:29 +0100

    Add Local ENS support on migration (#18)
    * Initial ENS setup wallet naming
    
    * Add example naming for governance
    
    * Register all deployed contracts with ENS
    
    * Lint the updated migrations
    
    * Initial ENS setup wallet naming
    
    * Register all deployed contracts with ENS
    
    * Lint the updated migrations
    
    * Bump version to 0.2.0 for ens feature
    
    * Remove node version 8 as causing issues
    
    * Put version in quotes
    
    * Fix npm start and npm test by ignoring the preflight check
    
    * Add quotes to node-version in frontend CI config
    
    * Remove unnecessary backend tests for Users and VersionResolver as these contracts no longer exist
    
    * Implement ENSResolver and import ENSRegistry and FIFSRegistrar
    
    * Change migrations to use ENSResolver and imported Registry, Registrar + Write test for ENSResolver
    
    * Added new line at end of file

---
+++ migrations/3_deploy_governance.js
@@ -0,0 +1,17 @@
+const Governance = artifacts.require('Governance');
+const ENSResolver = artifacts.require('ENSResolver');
+const FIFSRegistrar = artifacts.require('FIFSRegistrar');
+
+const newLabel = require('./helper/LoggedRegistration');
+
+module.exports = async (deployer, network, accounts) => {
+  await deployer.deploy(Governance, { from: accounts[0] });
+
+  await newLabel(
+    'governance',
+    accounts[0],
+    await ENSResolver.deployed(),
+    await FIFSRegistrar.deployed(),
+    await Governance.deployed()
+  );
+};
