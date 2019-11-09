3bc9a62d946accd5e9ddb692177fad2ef3fa3147
JamesDalboth <james.dalboth@gmail.com>
2019-11-09 14:57:50 +0000

    Change ENS name

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,8 +1,5 @@
 const Migrations = artifacts.require('Migrations');
 
 module.exports = (deployer) => {
-  console.log(process.env.INFURA_API_KEY);
-  console.log(process.env.WALLET_MNEMONIC);
-  console.log(process.env.ACCOUNT_ADDRESS);
   deployer.deploy(Migrations);
 };
