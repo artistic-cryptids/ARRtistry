84b97d1559d60a9b7c571cac7e4e8d79beab2ff0
Tooni <sayers.neil@gmail.com>
2019-11-09 15:48:39 +0000

    Removed random letter from index.tsx

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -12,7 +12,6 @@ import './theme.scss';
 
 // eslint-disable-next-line
 const Web3 = require('web3');
-
 // eslint-disable-next-line
 const contract = require('@truffle/contract');
 
@@ -20,7 +19,7 @@ const doDapp = async (): Promise<void> => {
   const web3 = new Web3(Web3.givenProvider || 'ws://127.0.0.1:8545');
   const provider = web3.currentProvider;
   const governanceNonDeployed = contract(Governance);
-  const artifactApplicationNonDeployed = contract(ArtifactApplication);y
+  const artifactApplicationNonDeployed = contract(ArtifactApplication);
   const artifactRegistryNonDeployed = contract(ArtifactRegistry);
   const artistsNonDeployed = contract(Artists);
   governanceNonDeployed.setProvider(provider);
