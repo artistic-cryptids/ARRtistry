47333fa2b44e09799c498ad77d485965c13785a3
Matthew Malarkey <mm5917@ic.ac.uk>
2019-11-12 21:19:46 +0000

    Add function to encrypt copies of plaintext for multiple people.

--- app/src/helper/encrypt.ts
+++ app/src/helper/encrypt.ts
@@ -1,11 +1,22 @@
 import * as EthCrypto from 'eth-crypto';
 
+type IpfsEncryptedDocument = EthCrypto.Encrypted;
+
 // Encrypt plaintext with the public key of who we want to be able to decrypt it
-export async function encrypt (publicKey: string, plaintext: string): Promise<EthCrypto.Encrypted> {
+export async function encrypt (publicKey: string, plaintext: string): Promise<IpfsEncryptedDocument> {
   return EthCrypto.encryptWithPublicKey(publicKey, plaintext);
 }
 
 // Decrypt the encrypted object with the private key corresponding to the public key it was encrypted with
-export async function decrypt (privateKey: string, ciphertext: EthCrypto.Encrypted): Promise<string> {
+export async function decrypt (privateKey: string, ciphertext: IpfsEncryptedDocument): Promise<string> {
   return EthCrypto.decryptWithPrivateKey(privateKey, ciphertext);
+}
+
+// Given a json document to encrypt and a list of public keys of people to encrypt it for,
+export async function encryptForIpfs (publicKeys: string[], plaintext: string): Promise<IpfsEncryptedDocument[]> {
+  const encrypted: IpfsEncryptedDocument[] = [];
+  for (const key of publicKeys) {
+    encrypted.push(await encrypt(key, plaintext));
+  }
+  return encrypted;
 }
--- app/src/test/encrypt.test.ts
+++ app/src/test/encrypt.test.ts
@@ -1,18 +1,35 @@
 import * as EthCrypto from 'eth-crypto';
-import { decrypt, encrypt } from '../helper/encrypt';
+import { decrypt, encrypt, encryptForIpfs } from '../helper/encrypt';
+
+const plaintext = 'I would like to encrypt this';
 
 it('encrypts the plaintext', async () => {
-  const plaintext = 'I would like to encrypt this';
-  const alice = EthCrypto.createIdentity();
   const bob = EthCrypto.createIdentity();
   expect((await encrypt(bob.publicKey, plaintext)).ciphertext).not.toBe(plaintext);
 });
 
 it('can decrypt the plaintext correctly', async () => {
-  const plaintext = 'I would like to encrypt this';
-  const alice = EthCrypto.createIdentity();
   const bob = EthCrypto.createIdentity();
-
   const encrypted = await encrypt(bob.publicKey, plaintext);
   expect(await decrypt(bob.privateKey, encrypted)).toBe(plaintext);
+});
+
+it('can encrypt for multiple people', async () => {
+  const bob = EthCrypto.createIdentity();
+  const charlie = EthCrypto.createIdentity();
+  const encrypted = await encryptForIpfs([bob.publicKey, charlie.publicKey], plaintext);
+
+  expect(encrypted.length).toBe(2);
+  expect(encrypted[0].ciphertext).not.toBe(plaintext);
+  expect(encrypted[1].ciphertext).not.toBe(plaintext);
+  expect(encrypted[0].ciphertext).not.toBe(encrypted[1].ciphertext);
+});
+
+it('can be decrypted by the correct people', async () => {
+  const bob = EthCrypto.createIdentity();
+  const charlie = EthCrypto.createIdentity();
+  const encrypted = await encryptForIpfs([bob.publicKey, charlie.publicKey], plaintext);
+
+  expect(await decrypt(bob.privateKey, encrypted[0])).toBe(plaintext);
+  expect(await decrypt(charlie.privateKey, encrypted[1])).toBe(plaintext);
 });
