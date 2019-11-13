1aced4b00f55afd7520a84151de48fbe8cc87979
Daniel Hails <daniel@hails.info>
2019-11-13 21:38:22 +0000

    Add Userbar & Contextual headings (#68)
    * Migrate variables to configuration
    
    * Remove unnecessary height for network aside
    
    * Update homepage with header bar
    
    * Create Main component and wrap views with it
    
    * Remove old HomePageNoAccount
    
    * Small style improvements
    
    * Update frontend CI to allow up to 4GB for Node as building was running out of memory
    
    * Add alt messages and remove old href
    
    * Add dummy href
    
    * Add missing trailing comma

--- app/src/views/ProposalView.tsx
+++ app/src/views/ProposalView.tsx
@@ -2,14 +2,17 @@ import * as React from 'react';
 
 import ProposalList from '../components/ProposalList';
 import ModeratorOnly from '../components/ModeratorOnly';
+import Main from '../components/Main';
 import { ContractProps } from '../helper/eth';
 
 const ProposalView: React.FC<ContractProps> = (props) => {
   const { contracts, accounts } = props;
   return (
-    <ModeratorOnly contracts={contracts} accounts={accounts}>
-      <ProposalList contracts={contracts} accounts={accounts}/>
-    </ModeratorOnly>
+    <Main page="Artifact Requests" parents={['Management']}>
+      <ModeratorOnly contracts={contracts} accounts={accounts}>
+        <ProposalList contracts={contracts} accounts={accounts}/>
+      </ModeratorOnly>
+    </Main>
   );
 };
 
