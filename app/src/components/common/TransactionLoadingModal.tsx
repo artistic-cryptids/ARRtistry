a249c2b040a16563436239ac8c1b9b97f5064d2f
Neil Sayers <sayers.neil@gmail.com>
2019-11-13 16:46:05 +0000

    Fix/loading bars (#65)
    * Replaced loading bars with basic spinner, and made sure it closes on Rejections
    
    * Lint
    
    * Accounted for more cases where the loading spinner ought to disappear

--- app/src/components/common/TransactionLoadingModal.tsx
+++ app/src/components/common/TransactionLoadingModal.tsx
@@ -7,26 +7,15 @@ interface TransactionLoadingModalProps {
   onHide: () => void;
   submitted: boolean;
   title: string;
-  transactionStackId: any;
 }
 
 class TransactionLoadingModal extends React.Component<TransactionLoadingModalProps, {}> {
-  SUBMISSION_STARTED = 10;
-  TRANSACTION_REGISTERED = 30;
-  TRANSACTION_APPROVED = 60;
-  SUBMISSION_FINISHED = 100;
-
-  progress = (): number => {
-    return 50;
-  };
-
   render (): React.ReactNode {
     return (
       <Fade in={this.props.submitted}>
         <LoadingModal
           show={this.props.submitted}
           onHide={this.props.onHide}
-          progress={this.progress()}
           title={this.props.title}
         />
       </Fade>
