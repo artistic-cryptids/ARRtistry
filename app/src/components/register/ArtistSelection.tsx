364ff64f2f25124721d100f36458e78b494b858b
DJRHails <daniel@hails.info>
2019-11-14 21:46:56 +0000

    Use option value rather than looking up artist name each time

--- app/src/components/register/ArtistSelection.tsx
+++ app/src/components/register/ArtistSelection.tsx
@@ -1,33 +1,20 @@
 import { useArtistContext, Artist } from '../../providers/ArtistProvider';
-import { SetValueContext, InputChangeEvent } from './RegisterForm';
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
+import { useFormControlContext } from '../../providers/FormProvider';
+import { FormControlProps } from 'react-bootstrap/FormControl';
 
 const ArtistSelection: React.FC = () => {
   const { artists } = useArtistContext();
-  const setValue = React.useContext(SetValueContext);
+  const { setField } = useFormControlContext();
 
   const artistOptions = artists.map((artist: Artist, id: number) => {
-    return <option key={id}>{artist.name}</option>;
+    return <option key={id} value={artist.id}>{artist.name}</option>;
   });
 
-  const onArtistChange = (event: InputChangeEvent): void => {
-    const value = event.target.value;
-
-    let artist;
-    for (const a of artists) {
-      if (a.name === value) {
-        artist = a;
-        break;
-      }
-    }
-
-    if (!artist) {
-      return;
-    }
-
-    setValue('artistId', artist.id.toString());
+  const onArtistChange = (event: React.FormEvent<HTMLSelectElement & FormControlProps>): void => {
+    setField('artistId', event.currentTarget.value.toString());
   };
 
   return (
