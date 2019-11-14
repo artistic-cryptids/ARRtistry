b06213b5813caa47cb0e07e38cfafed818828aac
DJRHails <daniel@hails.info>
2019-11-14 21:45:05 +0000

    Make IPFS more useful

--- app/src/helper/ipfs.ts
+++ app/src/helper/ipfs.ts
@@ -2,6 +2,15 @@
 const IpfsClient = require('ipfs-http-client');
 // equivalent import statement does not work.
 
+export const IPFS_URL_START = 'https://ipfs.io/ipfs/';
+
+export interface IpfsResponse {
+  hash: string;
+}
+
+type Uploadable = (string | ArrayBuffer | null);
+type HashCallback = (hash: string) => void;
+type ResponseCallback = (response: IpfsResponse[]) => void;
 // create a new ipfs client pointing to infura
 const ipfs = new IpfsClient({
   host: 'ipfs.infura.io',
@@ -9,10 +18,24 @@ const ipfs = new IpfsClient({
   protocol: 'https',
 });
 
-export const saveToIPFS = async (files: any, callback: (hash: string) => void): Promise<void> => {
-  const responseArray = await ipfs.add([...files], { progress: (prog: any) => console.log(`received: ${prog}`) });
-  console.log(responseArray);
-  callback(responseArray[0].hash);
+const progressMonitor = (prog: number): void => console.log(`received: ${prog}`);
+
+export const saveSingleToIPFS = async (files: Uploadable, callback: HashCallback): Promise<void> => {
+  const responseArray = await ipfs.add(files, { progress: progressMonitor });
+  if (responseArray.length > 0) {
+    callback(responseArray[0].hash);
+  } else {
+    console.error('IPFS failed to return', responseArray);
+  }
+};
+
+export const saveToIPFS = async (files: Uploadable[], callback: ResponseCallback): Promise<void> => {
+  const responseArray: IpfsResponse[] = await ipfs.add(files, { progress: progressMonitor });
+  if (responseArray.length > 0) {
+    callback(responseArray);
+  } else {
+    console.error('IPFS failed to return', responseArray);
+  }
 };
 
 export default ipfs;
