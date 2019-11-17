538ef1e0f3ba86530c64344a18922deb3c583224
Tooni <sayers.neil@gmail.com>
2019-11-17 20:58:19 +0000

    Added rinkeby names and addresses to hardcoded address-name mapping

--- app/src/helper/ensResolver.ts
+++ app/src/helper/ensResolver.ts
@@ -10,6 +10,12 @@ export const addressFromName = async (ens: any, name: string): Promise<string> =
   return addr; */
 
   switch (name) {
+  // rinkeby
+  case 'deployer.dacs.test': return '0x594cd738A5e99134De9DE21f253eD1Be4eb27F3e';
+  case 'dealer.matthew.test': return '0xbc70bAD29B7f37903bCaba13bA513Caa69a90ba6';
+  case 'artist.harry.test': return '0x911C76dF247cd605Df62A8dC09ab80e128d7A9E2';
+  case 'moderator.james.test': return '0x39be10202A6Aa302e344086A1f5dB4f09499B129';
+  // ganache
   case 'artistic.test': return '0xDf08F82De32B8d460adbE8D72043E3a7e25A3B39';
   case 'acc1.test': return '0x6704Fbfcd5Ef766B287262fA2281C105d57246a6';
   case 'acc2.test': return '0x9E1Ef1eC212F5DFfB41d35d9E5c14054F26c6560';
@@ -26,6 +32,12 @@ export const addressFromName = async (ens: any, name: string): Promise<string> =
 // eslint-disable-next-line
 export const nameFromAddress = async (ens: any, address: string): Promise<string> => { 
   switch (address) {
+  // rinkeby
+  case '0x594cd738A5e99134De9DE21f253eD1Be4eb27F3e': return 'deployer.dacs.test';
+  case '0xbc70bAD29B7f37903bCaba13bA513Caa69a90ba6': return 'dealer.matthew.test';
+  case '0x911C76dF247cd605Df62A8dC09ab80e128d7A9E2': return 'artist.harry.test';
+  case '0x39be10202A6Aa302e344086A1f5dB4f09499B129': return 'moderator.james.test';
+  // ganache
   case '0xDf08F82De32B8d460adbE8D72043E3a7e25A3B39': return 'artistic.test';
   case '0x6704Fbfcd5Ef766B287262fA2281C105d57246a6': return 'acc1.test';
   case '0x9E1Ef1eC212F5DFfB41d35d9E5c14054F26c6560': return 'acc2.test';
