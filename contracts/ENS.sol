9c4ee9209117fedcfd074311c32cf49a401f0589
James Dalboth <James.Dalboth@gmail.com>
2019-11-17 21:34:22 +0000

    Local Reverse Registrar (#84)
    * Fix migrations to correctly register an address
    
    * Fix linting
    
    * Deploy reverse registrar
    
    * Add reverse lookup to resolver
    
    * Provide default name <-> addr mappings for development
    
    * Linting fixes
    
    * Added test for ENSResolver
    
    * add interface for name
    
    * Fix merge issues
    
    * Linting fixes

--- contracts/ENS.sol
+++ contracts/ENS.sol
@@ -2,15 +2,18 @@ pragma solidity 0.5.12;
 
 import "@ensdomains/ens/contracts/ENSRegistry.sol";
 import "@ensdomains/ens/contracts/FIFSRegistrar.sol";
+import "@ensdomains/ens/contracts/ReverseRegistrar.sol";
 
 contract ENSResolver {
 
   mapping(bytes32 => address) public addresses;
+  mapping(bytes32 => string) public names;
 
-  // Our resolver supports (supportsInterface & addr) interface
+  // Our resolver supports (supportsInterface & addr & name) interface
   function supportsInterface(bytes4 interfaceID) public pure returns (bool) {
     return interfaceID == 0x3b3b57de
-    || interfaceID == 0x01ffc9a7;
+    || interfaceID == 0x01ffc9a7
+    || interfaceID == 0x691f3431;
   }
 
   // TODO(mm): remove this hint disable
@@ -21,5 +24,13 @@ contract ENSResolver {
 
   function setAddr(bytes32 nodeID, address addr) external {
     addresses[nodeID] = addr;
+  }
+
+  function setName(bytes32 node, string memory name) public {
+    names[node] = name;
+  }
+
+  function name(bytes32 node) public view returns (string memory) {
+    return names[node];
   }
 }
