f04043e39adda442f35dc78a8441dc9d5781c3fe
Neil Sayers <sayers.neil@gmail.com>
2019-11-17 23:05:54 +0000

    Hardcoded name/address mappings used everywhere relevant, to stop addresses showing. (#83)
    * Added extra address to ENS name mappings for Ganache deployment
    
    * passed ens object to everything. implemented ens use for artifact transfer
    
    * Used names for consignment and ARR listings too
    
    * Added semi-colons to registrar helper, for linting
    
    * Create ENSName component to display addresses
    
    * Linted ensResolver.ts
    
    * lint
    
    * basic connection of name to sessionprovider
    
    * made provenance convert addresses to ens names
    
    * Made Provenance use ENSName component
    
    * Fixed ARRItem to use ENSName too
    
    * Added rinkeby names and addresses to hardcoded address-name mapping
    
    * Attempted to make the requested changes

--- app/src/components/common/ENSName.tsx
+++ app/src/components/common/ENSName.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import { ContractProps } from '../../helper/eth';
+import { nameFromAddress } from '../../helper/ensResolver';
 
 interface ENSNameProps extends ContractProps {
   address: string;
@@ -14,7 +15,7 @@ interface ENSNameState {
 class ENSName extends React.Component<ENSNameProps, ENSNameState> {
   componentDidMount (): void {
     // Where we would normally check ENS for names
-    let name;
+    /* let name;
     switch (this.props.address) {
     case '0xDf08F82De32B8d460adbE8D72043E3a7e25A3B39':
       name = 'dacs.arrtistry.test';
@@ -27,8 +28,12 @@ class ENSName extends React.Component<ENSNameProps, ENSNameState> {
       break;
     default:
       name = 'default.arrtistry.test';
-    }
+    } */
+    this.getName();
+  }
 
+  getName = async (): Promise<void> => {
+    const name = await nameFromAddress(this.props.contracts.Ens, this.props.address);
     this.setState({ name: name });
   }
 
