66570e27d4b340151ef116c0005d3856708c7b6f
matthew-ho <34010372+matthew-ho@users.noreply.github.com>
2019-11-21 13:39:10 +0000

    Modify default artist list to be artists which are under copyright and represented by DACS (#98)

--- migrations/7_deploy_artists.js
+++ migrations/7_deploy_artists.js
@@ -12,14 +12,14 @@ module.exports = async (deployer, network, accounts) => {
   const artists = await Artists.deployed();
 
   // Some prefiled in files with artist infos
-  await artists.addArtist('https://ipfs.globalupload.io/Qmdgp81Wd8NhMgzHSYzWyN4FAP4CPTwtiKopznoNzyfyy8');
+  await artists.addArtist('https://ipfs.globalupload.io/QmcSJUtkFPad6DvsM7TQMF2GP9mnCg1heshGEMPk9rmURb');
   await artists.addArtist('https://ipfs.globalupload.io/QmdBkQ31t6MBKZAFAq56mbQf3bb1qdjdssqjVVNXZK5SJv');
-  await artists.addArtist('https://ipfs.globalupload.io/QmeribWDZU9rBJJesgAZowP2GmvQ4NSTPCjuqM6rtrRb8e');
-  await artists.addArtist('https://ipfs.globalupload.io/QmeYjgK9LpijLWTGki3J9VZNj6R5c93m5enHXH4Ezqg1JN');
+  await artists.addArtist('https://ipfs.globalupload.io/QmRT1LTLJKQpd5xDYUXCdpXos5x1Wj2tFQLgBsXU6BkyQV');
+  await artists.addArtist('https://ipfs.globalupload.io/QmcFDXonNo9LqJR1wBFWWPN2HUKeoQ5YNDmKo5pY3xWgu1');
   await artists.addArtist('https://ipfs.globalupload.io/Qmd5Hd5G4tM28ErZr1VvdgV7oYebyavZ5jCn593JwWhuud');
   await artists.addArtist('https://ipfs.globalupload.io/Qmb2vvBVvGaXoqYEECYmyKbxpCgSbFVAPy6kCPrRCdLRPK');
-  await artists.addArtist('https://ipfs.globalupload.io/QmXJ5tg8ob5XQgZvK5uTZ5pEieKhF9JpFSYdQJpNsmb7dU');
-  await artists.addArtist('https://ipfs.globalupload.io/QmfSLSJoxqktyEKN4useBRPAPaJ9ENFFXmiYX5BQtm4Qu7');
+  await artists.addArtist('https://ipfs.globalupload.io/QmXiWt1yjfmLDLAHUNyoFNiPf3BfpXWqUWR2iawE9Mrsds');
+  await artists.addArtist('https://ipfs.globalupload.io/QmUtqRy5FUokety8mpTivmhFqZ8D5ZRd9FFhp63qZKN5uv');
 
   await newLabel(
     'artists',
