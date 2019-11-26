f1b31013ec4d897aa1ff97b3296586a0f87a4b6c
JamesDalboth <james.dalboth@gmail.com>
2019-11-26 14:59:25 +0000

    Extend set of locations to include all possible locations

--- app/src/components/ArtworkItem.tsx
+++ app/src/components/ArtworkItem.tsx
@@ -27,7 +27,7 @@ const ArtworkItem: React.FC<ArtworkItemProps> = ({ tokenId, ownedArtifact, fulls
         setArtwork(artwork);
       })
       .catch(console.log);
-  }, [ArtifactRegistry, tokenId]);
+  }, [ArtifactRegistry.methods, tokenId]);
 
   if (!artwork) {
     return <ArtworkCard img='https://file.globalupload.io/HO8sN3I2nJ.png'/>;
