dd1abfa79b07bcfd8745c8859363c843b8c1b116
Matthew Ho <matthew_ho@hotmail.co.uk>
2019-11-28 15:44:42 +0000

    Fix artistId not being set by adding setFields which allows multiple fields to be set at the same time

--- app/src/providers/FormProvider.tsx
+++ app/src/providers/FormProvider.tsx
@@ -53,10 +53,12 @@ export const DEFAULT_FORM_STATUS: FormStatus = {
 };
 
 type SetField = (name: string, value: string) => void;
+type SetFields = (newFields: {[key: string]: string}) => void;
 
 export interface FormControl {
   status: FormStatus;
   setField: SetField;
+  setFields: SetFields;
   clear: VoidFunction;
 }
 
@@ -64,6 +66,7 @@ const TextFieldContext = React.createContext<TextFields>(DEFAULT_TEXT_FIELDS);
 const FormControlContext = React.createContext<FormControl>({
   status: DEFAULT_FORM_STATUS,
   setField: console.warn,
+  setFields: (newFields: {[key: string]: string}) => console.warn(newFields),
   clear: console.warn,
 });
 const ErrorsContext = React.createContext<ErrorMessages>(DEFAULT_ERRORS);
@@ -85,6 +88,19 @@ export const FormProvider: React.FC<FormProviderProps> = ({ onSubmit, validator,
     setTextFields({
       ...textFields,
       [id]: value,
+    });
+  };
+
+  /* _setFields should be used if multiple fields need to be set at a similar time.
+   * This is because React's setState is asynchronous so the textFields retrieved
+   * when setting the new fields may not update in time if setField is called
+   * multiple times in a short time period.
+   *
+   */
+  const _setFields = (newFields: {[key: string]: string}): void => {
+    setTextFields({
+      ...textFields,
+      ...newFields,
     });
   };
 
@@ -116,7 +132,12 @@ export const FormProvider: React.FC<FormProviderProps> = ({ onSubmit, validator,
 
   return (
     <TextFieldContext.Provider value={textFields}>
-      <FormControlContext.Provider value={{ status: formStatus, setField: _setField, clear: clearForm }}>
+      <FormControlContext.Provider value={{
+        status: formStatus,
+        setField: _setField,
+        setFields: _setFields,
+        clear: clearForm,
+      }}>
         <ErrorsContext.Provider value={errors}>
           <Form
             noValidate
