7f2837da241db3e243405333f25da92b108fad09
matthew-ho <34010372+matthew-ho@users.noreply.github.com>
2019-12-01 15:00:37 +0000

    Modify artifact requests and client artifact views to listen to events so they responsive (#120)

--- app/src/components/ClientArtifacts.tsx
+++ app/src/components/ClientArtifacts.tsx
@@ -14,24 +14,44 @@ const ClientArtifacts: React.FC = () => {
   const [numClientArtifacts, setNumClientArtifacts] = React.useState<number>();
   const [tokenIds, setTokenIds] = React.useState<number[]>();
 
-  const { Consignment } = useContractContext();
+  const { Consignment, ArtifactRegistry } = useContractContext();
   const { accounts } = useWeb3Context();
 
   React.useEffect(() => {
-    Consignment.methods.consignedTokenIds()
-      .call({
-        from: accounts[0],
-      })
-      .then((tokenIdObjects: any) => {
-        const tokenIds: number[] = [];
-        tokenIdObjects.map((tid: any) => tokenIds.push(tid));
-        if (tokenIds.length !== numClientArtifacts) {
-          setNumClientArtifacts(tokenIds.length);
-          setTokenIds(tokenIds);
-        }
-      })
-      .catch(console.log);
-  }, [Consignment, accounts, numClientArtifacts]);
+    const subscribeToTransfer: VoidFunction = () => {
+      const updateTokenIds = (): void => {
+        Consignment.methods.consignedTokenIds()
+          .call({
+            from: accounts[0],
+          })
+          .then((tokenIdObjects: any) => {
+            const tokenIds: number[] = [];
+            tokenIdObjects.map((tid: any) => tokenIds.push(tid));
+            if (tokenIds.length !== numClientArtifacts) {
+              setNumClientArtifacts(tokenIds.length);
+              setTokenIds(tokenIds);
+            }
+          })
+          .catch(console.log);
+      };
+
+      updateTokenIds();
+
+      const transferSubscription = ArtifactRegistry.events.Transfer()
+        .on('data', (_: any) => {
+          updateTokenIds();
+        });
+
+      return () => {
+        // Unsubscribe when this component is unmounted
+        transferSubscription.unsubscribe();
+      };
+    };
+
+    if (ArtifactRegistry && accounts.length > 0) {
+      subscribeToTransfer();
+    }
+  }, [ArtifactRegistry, accounts, Consignment, numClientArtifacts]);
 
   if (!numClientArtifacts) {
     return (
