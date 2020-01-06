295574d7d68779cd747283a6191636776ab4b16b
felination <harryjrblack@yahoo.co.uk>
2020-01-06 13:00:00 +0000

    Correct migration script to function with Rinkeby

--- migrations/8_add_artifacts.js
+++ migrations/8_add_artifacts.js
@@ -2,8 +2,8 @@ const ArtifactRegistry = artifacts.require('ArtifactRegistry');
 
 module.exports = async (deployer, network, accounts) => {
   const registry = await ArtifactRegistry.deployed();
-  await registry.mint(accounts[0], {
-    artist: accounts[0],
+  await registry.mint(getOwner(), {
+    artist: getOwner(),
     metaUri: 'https://ipfs.io/ipfs/QmRGqMQB2oaHN4dnK4uF4Xaz5Eevicvttp7fZmPjWTKr3N',
   });
 };
