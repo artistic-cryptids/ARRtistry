37218430775d4f88c14bc06468366300a9ecfcb0
felination <harryjrblack@yahoo.co.uk>
2020-01-06 13:06:49 +0000

    Correct error expected by ERC20 test

--- test/behaviours/ERC20.behavior.js
+++ test/behaviours/ERC20.behavior.js
@@ -106,7 +106,7 @@ function shouldBehaveLikeERC20 (errorPrefix, initialSupply, initialHolder, recip
 
             it('reverts', async function () {
               await expectRevert(this.token.transferFrom(
-                tokenOwner, to, amount, { from: spender }), `${errorPrefix}: transfer amount exceeds allowance`,
+                tokenOwner, to, amount, { from: spender }), `${errorPrefix}: transfer amount exceeds balance`,
               );
             });
           });
