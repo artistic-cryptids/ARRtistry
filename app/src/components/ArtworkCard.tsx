1d65c8514409e3b0f8a2c2f8ad0e98655b0faff2
felination <harryjrblack@yahoo.co.uk>
2020-01-08 16:32:49 +0000

    Add null check when timestamping an artwork card, for the case when a preloaded art piece's JSON cannot be read

--- app/src/components/ArtworkCard.tsx
+++ app/src/components/ArtworkCard.tsx
@@ -56,11 +56,13 @@ const ArtworkCard: React.FC<ArtworkCardProps> = ({
           .then((es: EventData[]) =>
             es.filter(e => e.returnValues.proposalId === id.toString()));
       }
+      if (events.length > 0) {
       const event = events[events.length - 1];
       const timestamp = await web3.eth.getBlock(event.blockNumber)
         .then((block) => block.timestamp);
       const txDate = moment.unix(Number(timestamp));
       setUpdateTime('Last Updated ' + txDate.fromNow());
+    }
     };
     getLastUpdated();
   }, [ArtifactRegistry, id, web3, Governance]);
