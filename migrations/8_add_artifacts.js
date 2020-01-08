62be82c964062995bfb7d2ba76d15a91e73d608e
JamesDalboth <james.dalboth@gmail.com>
2020-01-08 16:28:21 +0000

    Whoops bang bong

--- migrations/8_add_artifacts.js
+++ migrations/8_add_artifacts.js
@@ -3,8 +3,8 @@ const ArtifactRegistry = artifacts.require('ArtifactRegistry');
 
 module.exports = async (deployer, network, accounts) => {
   const registry = await ArtifactRegistry.deployed();
-  await registry.mint(getOwner(network, accounts), {
-    artist: getOwner(network, accounts),
-    metaUri: 'https://ipfs.io/ipfs/QmRGqMQB2oaHN4dnK4uF4Xaz5Eevicvttp7fZmPjWTKr3N',
-  });
+  // await registry.mint(getOwner(network, accounts), {
+  //   artist: getOwner(network, accounts),
+  //   metaUri: 'https://ipfs.io/ipfs/QmRGqMQB2oaHN4dnK4uF4Xaz5Eevicvttp7fZmPjWTKr3N',
+  // });
 };
