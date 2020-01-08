ed64c9ae8201885bdc7a750a90e9e7f547ed487a
Matthew Malarkey <mm5917@ic.ac.uk>
2020-01-08 19:34:19 +0000

    Update ipfs urls with working image links

--- migrations/8_add_artifacts.js
+++ migrations/8_add_artifacts.js
@@ -3,21 +3,17 @@ const ArtifactRegistry = artifacts.require('ArtifactRegistry');
 
 module.exports = async (deployer, network, accounts) => {
   const registry = await ArtifactRegistry.deployed();
-  // await registry.mint(getOwner(network, accounts), {
-  //   artist: getOwner(network, accounts),
-  //   metaUri: 'https://ipfs.io/ipfs/QmRGqMQB2oaHN4dnK4uF4Xaz5Eevicvttp7fZmPjWTKr3N',
-  // });
 
   const artworkUris = [
     'https://ipfs.globalupload.io/QmWeBdyapreRbeWaistYV676DTfFcUF3LoMT6VfafojjdA',
-    'https://ipfs.globalupload.io/QmVW3XRsaSMM7ekMUtaeWokXr9SWQ1kojkZommjMBwsEvQ',
-    'https://ipfs.globalupload.io/QmNf3EpnFbVqFyMTYxyjAxUJPjEAoVchFSBSQ6CWuqds5u',
-    'https://ipfs.globalupload.io/QmTcyw7fHsZuXxHShJ5oy7iNevVrDnRNrrfXPWEBrvTQzf',
-    'https://ipfs.globalupload.io/QmWHb9TUJVmH2ue7LxCLSkZXJZGbpFPb5CeuZpMcdtD3ov',
-    'https://ipfs.globalupload.io/QmQ3DEujR26x8T7KSdBkEqc4uGUYnDeE52GsLfntLAbTxZ',
-    'https://ipfs.globalupload.io/QmbH8M6U5vbciUKYnn6q3HAoYA1gtxfL7HPn2BaVEQkar6',
-    'https://ipfs.globalupload.io/QmbC1uhoQsKEo2LRChMWt66LoLBxX7REyf9XNDxR5wtPAg',
-    'https://ipfs.globalupload.io/QmUMASwxDsmaZU2aST2Mwzw2S2sM7yWSE7DKhYP3p1Lfaq',
+    'https://ipfs.globalupload.io/QmQdmTNy6R2dYoTF8Q3PSnNZq1s7HghWpZXoyesUmJqzZ7',
+    'https://ipfs.globalupload.io/QmRMTQvmdziqkNzac1eJCBbAGm7RrejjzJTf9MfQsK8vGF',
+    'https://ipfs.globalupload.io/QmYJaMwhsURVmL8r1DeCE7XGEh52w8AFcVfhXbzQEXLw7c',
+    'https://ipfs.globalupload.io/QmZYdrEAQP8stXJmLVhndMLL7TKRzK2ss3aSQE8pGmn1un',
+    'https://ipfs.globalupload.io/QmY6WYMk6p1EaRtVjhknZHtG2WWL6xoTs4fjrzJitjbQPj',
+    'https://ipfs.globalupload.io/QmRb7PBb686kfPufan3nn4KwY6usVr2qEKPcRiF2jbsLqh',
+    'https://ipfs.globalupload.io/Qmd6t22n9YnxVdrgSwAAP68UJ58Q1pAYbGCV3aHwGnxwzi',
+    'https://ipfs.globalupload.io/QmNPh1JZdVRU6EvbosjiKMtC4i7RgikJ7sAwQXvboyKA61',
   ];
 
   artworkUris.forEach(async a => await registry.mint(getOwner(network, accounts), {
