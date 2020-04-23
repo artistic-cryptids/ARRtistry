6e15ce7199e18f78e9252ddb4abbfd96811bb490
DJRHails <daniel@hails.info>
2020-04-23 18:27:30 +0100

    More linting issues

--- app/src/components/ArrList.tsx
+++ app/src/components/ArrList.tsx
@@ -4,15 +4,14 @@ import ArrItem, { ArrItemType } from './ArrItem';
 import CardColumns from 'react-bootstrap/CardColumns';
 import { useContractContext } from '../providers/ContractProvider';
 
-
-const LoadingArrItem: React.FC<{id: number, wrappedARR: Promise<ArrItemType>}> = ({ id, wrappedARR}) => {
+const LoadingArrItem: React.FC<{id: number; wrappedARR: Promise<ArrItemType>}> = ({ id, wrappedARR }) => {
   const [arr, setArr] = React.useState<ArrItemType>();
 
   React.useEffect(() => {
     wrappedARR
       .then((arr) => setArr(arr))
       .catch(console.log);
-  }, [wrappedARR])
+  }, [wrappedARR]);
 
   if (!arr) {
     return null;
@@ -20,9 +19,8 @@ const LoadingArrItem: React.FC<{id: number, wrappedARR: Promise<ArrItemType>}> =
   return <ArrItem
     id={id}
     arr={arr}
-  />
-}
-
+  />;
+};
 
 const ArrList: React.FC = () => {
   const { ArrRegistry, RoyaltyDistributor } = useContractContext();
@@ -30,37 +28,38 @@ const ArrList: React.FC = () => {
 
   React.useEffect(() => {
     const addDueAmountToARRItem = (promisedArr: Promise<ArrItemType>): Promise<ArrItemType> => (
-    promisedArr.then((arr: ArrItemType) => (
+      promisedArr.then((arr: ArrItemType) => (
         RoyaltyDistributor.methods.calculateARR(arr.price).call()
-        .then(parseInt)
-        .then(
-          (due: number) => {
-            if (due === 0) {
-              throw Error('Do not display 0 ARR');
-            }
-            return {
-              from: arr.from,
-              to: arr.to,
-              tokenId: arr.tokenId,
-              price: arr.price / 100,
-              due: due / 100,
-              location: arr.location,
-              paid: arr.paid,
-            }
-          }
-        )
+          .then(parseInt)
+          .then(
+            (due: number) => {
+              if (due === 0) {
+                throw Error('Do not display 0 ARR');
+              }
+              return {
+                from: arr.from,
+                to: arr.to,
+                tokenId: arr.tokenId,
+                price: arr.price / 100,
+                due: due / 100,
+                location: arr.location,
+                paid: arr.paid,
+              };
+            },
+          )
       ))
     );
 
     const loadARRMetadata = (id: number): Promise<ArrItemType> => {
       const meta: Promise<ArrItemType> = ArrRegistry.methods.retrieve(id).call();
       return addDueAmountToARRItem(meta);
-    }
+    };
 
     ArrRegistry.methods.totalRecords().call()
-      .then((total: string) => _.range(1, parseInt(total)+1))
+      .then((total: string) => _.range(1, parseInt(total) + 1))
       .catch(console.error)
-      .then((ids: number[]) => setPromisedArr(ids.map(loadARRMetadata)));
+      .then((ids: number[]) => setPromisedArr(ids.map(loadARRMetadata)))
+      .catch(console.error);
   }, [ArrRegistry, RoyaltyDistributor]);
 
   const listItems = promisedArr.map((arr: Promise<ArrItemType>, id: number) =>
@@ -68,7 +67,7 @@ const ArrList: React.FC = () => {
       id={id}
       key={id}
       wrappedARR={arr}
-    />
+    />,
   );
 
   return (
