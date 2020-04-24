d7c5611122c7401f0fbb1265248d434280e66fb0
DJRHails <daniel@hails.info>
2020-04-24 11:27:47 +0100

    Update Default User

--- app/src/providers/SessionProvider.tsx
+++ app/src/providers/SessionProvider.tsx
@@ -5,66 +5,10 @@ import SplashScreen from '../components/SplashScreen';
 import { getUserListMetadata } from '../helper/agnostic';
 
 const DEFAULT_USER = {
-  nickname: 'John Doe',
-  img: 'https://file.globalupload.io/HO8sN3I2nJ.png',
-  role: 'ARTIST',
-  name: 'default.arrtistry.test',
-};
-
-export const DACS_DEFAULT: User = {
-  nickname: 'Anna Doe',
-  img: 'https://mdbootstrap.com/img/Photos/Avatars/img%20%2820%29.jpg',
-  role: 'DACS',
-  address: [
-    '0xDf08F82De32B8d460adbE8D72043E3a7e25A3B39',
-    '0x594cd738A5e99134De9DE21f253eD1Be4eb27F3e',
-    '0xE120e76EC013911018483677D1f264E101e92bf1',
-  ],
-  name: 'dac.arrtistry.test',
-};
-
-export const DEAL_DEFAULT: User = {
-  nickname: 'Gallery',
-  img: 'https://www.cavan-arts.com/uploads/1/2/2/7/122790076/img-4071_orig.jpg',
-  role: 'DEAL',
-  address: [
-    '0xdE164a54b441808DA5C448D85Ba2F0F6e271CC36',
-    '0x97A3FC5Ee46852C1Cf92A97B7BaD42F2622267cC',
-    '0xce42bdB34189a93c55De250E011c68FaeE374Dd3',
-  ],
-  name: 'gallery.arrtistry.test',
-};
-
-export const NATASHA: User = {
-  nickname: 'Natasha',
-  img: 'https://mdbootstrap.com/img/Photos/Avatars/img%20(17).jpg',
-  role: 'ARTIST',
-  address: ['0xc70eAc1d854E51FaFC7a487086624E79cEE6e843'],
-  name: 'natasha.arrtistry.test',
-};
-
-export const NICHOLA: User = {
-  nickname: 'Nichola Theakston',
-  img: 'https://i.imgur.com/u6NWt8i.jpg',
-  role: 'ARTIST',
-  address: ['0x67EDE48B355DA3fb5d5fB6e5964DaB9fDA56aADe'],
-  name: 'nichola.arrtistry.test',
-};
-
-export const LUXEMBURG: User = {
-  nickname: 'Rut Blees',
-  img: 'https://www.tate.org.uk/art/images/work/P/P20/P20268_9.jpg',
-  role: 'ARTIST',
-  address: ['0xf323B526cfEbf52c349dA8F4BB4d7e6EFA55F3a6'],
-  name: 'rut.arrtistry.test',
-};
-
-export const BUYER_DEFAULT: User = {
-  nickname: 'Buyer',
-  img: 'https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg',
-  role: 'COLLECTOR',
-  address: ['0x1bf078753937FB3e569C4c9724654d10cc8A7Fd7'],
-  name: 'buyer.arrtistry.test',
+  nickname: 'Unknown',
+  img: 'https://arweave.net/koGEvbLifVjKVqWRruecP040lNWr8M9cI2IqQ1eyZXo',
+  role: 'UNREGISTERED',
+  name: '',
 };
 
 export interface User {
@@ -91,7 +35,6 @@ export const SessionProvider: React.FC = ({ children }) => {
   const [user, setUser] = React.useState<User>(defaultUser);
   const [gotUser, setGotUser] = React.useState<boolean>(false);
 
-  // let users = [DACS_DEFAULT, DEAL_DEFAULT, NATASHA, NICHOLA, LUXEMBURG, BUYER_DEFAULT];
   let users: Array<User> = [];
   getUserListMetadata('8R5oVUsbTnhiJlkm56HVRcEHvc9YEG4Hr3YxOYw_gSg')
     .then((userList: Array<User>) => {
