c68f74e6be0aa18dc87259a9ee658bf1c68b2612
DJRHails <daniel@hails.info>
2020-04-26 19:43:50 +0100

    Add Jazzicon and use in the same way as metamask

---
+++ app/src/components/common/Jazzicon.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from 'react';
+import jazzicon from 'jazzicon';
+
+interface JazziconProps {
+  address: string;
+  className?: string;
+  diameter?: number;
+}
+
+function jsNumberForAddress (address: string) {
+  const addr = address.slice(2, 10);
+  const seed = parseInt(addr, 16);
+  return seed;
+}
+
+// Jazzicon library returns a HTMLDivElement, so needs wrapping
+const Jazzicon: React.FC<JazziconProps> = ({ address, className, diameter = 46 }) => {
+  const container = useRef<HTMLDivElement>(null);
+
+  const removeChildren = () => {
+    if (!container.current) { return; }
+
+    const { children } = container.current;
+    for (let i = 0; i < children.length; i++) {
+      container.current.removeChild(children[i]);
+    }
+  };
+
+  const appendJazzicon = () => {
+    if (!container.current) { return; }
+
+    const image = jazzicon(diameter, jsNumberForAddress(address));
+    container.current.appendChild(image);
+  };
+
+  useEffect(() => {
+    removeChildren();
+    appendJazzicon();
+  }, [address, diameter]);
+
+  return (
+    <div
+      className={className}
+      ref={container}
+    />
+  );
+};
+
+export default Jazzicon;
