c59a5a47624f76cbf33bb537c7ce4e72d9f02081
DJRHails <daniel@hails.info>
2020-04-26 23:39:35 +0100

    Stupid return type

--- app/src/components/common/Jazzicon.tsx
+++ app/src/components/common/Jazzicon.tsx
@@ -7,7 +7,7 @@ interface JazziconProps {
   diameter?: number;
 }
 
-function jsNumberForAddress (address: string): string {
+function jsNumberForAddress (address: string): number {
   const addr = address.slice(2, 10);
   const seed = parseInt(addr, 16);
   return seed;
